b1726fc52f6b5a1584accccf48edde46a19d8f8f
Yulia N <iuliia_nak@gm.com>
2021-10-07 10:02:36 +0300

    new object

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 include ('vendor/autoload.php');
-$planeta= new \Space\Planeta('Юпитер');
-echo $planeta->name;
-$content=new GuzzleHttp\Client();
+$planeta= new \Space\Planeta('Земля');
+echo "Планета ".$planeta->name."<br>\n";
+$tree= new \Repo\Baum();
+echo $tree->grow('10 m');
+/*
+ $content=new GuzzleHttp\Client();
 $response = $content->get('https://itea.ua/');
 echo $response->getBody()->getContents();
+ */
